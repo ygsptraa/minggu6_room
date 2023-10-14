6662e8e4e9c9358fd49bb29d3907e6c493bc1396
Rachmad Yoga Saputra <yogas910@gmail.com>
2023-10-14 18:38:50 +0700

    Di MainActivity onCreate, berikan kode untuk FAB dan tambahkan onClickListener

--- app/src/main/java/com/example/minggu6_room/MainActivity.kt
+++ app/src/main/java/com/example/minggu6_room/MainActivity.kt
@@ -6,9 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private val newWordActivityRequestCode = 1
@@ -24,10 +24,15 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        wordViewModel.allWords.observe(this, Observer { words ->
+        wordViewModel.allWords.observe(this){ words ->
             // Update the cached copy of the words in the adapter.
             words?.let { adapter.submitList(it) }
-        })
+        }
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
