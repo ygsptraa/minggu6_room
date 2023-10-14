320a790c0e6f4ba747876e610c8fa27ab6faaa77
Rachmad Yoga Saputra <yogas910@gmail.com>
2023-10-14 18:16:34 +0700

    Buat Android Activity baru, memperbarui file activity_new_word.xml dengan kode baru

---
+++ app/src/main/java/com/example/minggu6_room/NewWordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.minggu6_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewWordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
